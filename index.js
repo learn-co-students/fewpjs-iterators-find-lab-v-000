9ec2d99e10ee395c657d0bb6251184d0d0ec6c81
Amanda Murphy <helloamandamurphy@gmail.com>
2019-10-12 14:58:19 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let winning_year = array.find(array => array.result === "W")
+  return !!winning_year ? winning_year.year : undefined
+}
