52a455e4cda315dabf26cc1d4268b7732ab89eaf
epic0448 <samuelogundiran@Yahoo.com>
2019-10-16 18:29:13 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  
+]
+function superbowlWin(array) {
+  let date = array.find(array => array.result === "W")
+  return !!date ? date.year: undefined
+}
