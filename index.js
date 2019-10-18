847440d0f739a6e475f09ce7d16d240eecf7f833
Kazumi Karbowski <kazkazinus@gmail.com>
2019-10-18 19:04:14 -0400

    Deleted space

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function testFunc() {
   return "hi"
 }
 
+
 const superbowlWin = record => {
   let result = record.find(record => record.result === "W")
   return !!result ? result.year : undefined
