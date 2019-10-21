603bc8ea69cc1cffc67def28928b5cbb76a07327
github username <github email address>
2019-10-20 23:09:37 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ]
+const superbowlWin = (record) => {
+	
+	const winYear = record.find(record => record.result === 'W' )
+	
+	
+	// if (winYear !== null ) {
+	// 	return winYear.year
+	// } else {
+	// 	return undefined
+	// }
+
+	if (!!winYear) {
+		return winYear.year
+	} else {
+		return undefined
+	}
 }
