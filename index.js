98130400048193f56de7828cf26789d696c0bb9d
Emi Ko <emi8emi@gmail.com>
2019-10-25 15:31:17 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (y)=> {
+  let result = y.find(y => y.result === "W"
+  )
+  return !!result ? result.year : undefined
+}
