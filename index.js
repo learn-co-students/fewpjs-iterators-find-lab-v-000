db2cad66745b3c9f752c6ede76dbd3872b91cc46
atxrenegade <harleighabel@gmail.com>
2019-10-26 14:47:43 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (statsArray) => {
+	return statsArray.find( element => {
+		if (element.result === "W"){
+			return element.year
+		} else {
+			return undefined;
+		}
+	})
+}
