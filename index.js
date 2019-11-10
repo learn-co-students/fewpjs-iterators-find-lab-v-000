bf5438e99cfcae321297e1ce6dee6b9ef406608d
John Blank <veloblank@gmail.com>
2019-11-10 06:23:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(arr) {
+  let bowlWin = arr.find(el => el['result'] === 'W')
+  if (bowlWin) {
+    return bowlWin.year
+  }
 }
