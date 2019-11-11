1f568e338688529da306c8f6c3136d13e5dcdc4f
AlwinaO <aoyewole2586@gmail.com>
2019-11-11 12:42:00 -0500

    Done.

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ function testFunc() {
   return "hi"
 }
 
-function superbowlWin(record) {
-  // remember to save value to variable after the computer checks for element
-  // then add variable to if condition
-  // output of condition is stored in value and can return corresponding value
-  let win = record.find(record => record.result === "W")
+// function superbowlWin(record) {
+//   // remember to save value to variable after the computer checks for element
+//   // then add variable to if condition
+//   // output of condition is stored in value and can return corresponding value
+//   let win = record.find(record => record.result === "W")
+//   if (win) {
+//     return win.year
+//   }
+// }
+
+const superbowlWin = function(record) {
+  let win = record.find(record => record.result === "W");
   if (win) {
-    return win.year
+  return win.year;
   }
 }
-
-// const superbowlWin = function(record) {
-//   record.find(r => r.result === "W")
-//   return r.year;
-// }
 
