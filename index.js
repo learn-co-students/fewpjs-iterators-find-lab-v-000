2b96671ad9f92dc865f5cbe39a6fc2fa7824274e
AlwinaO <aoyewole2586@gmail.com>
2019-11-11 11:38:36 -0500

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function testFunc() {
   return "hi"
 }
 
-function superBowlWin(record) {
-  record.find(result => result === "W")
-  return record.year;
-}
+// function superBowlWin(record) {
+//   record.find(result => result === "W")
+//   return record.year;
+// }
+
+const superBowlWin = record.find(r => r.result === "W" )
