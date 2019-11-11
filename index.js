0d7247918d25ae2f80e4ff2891d3432f0cf65731
AlwinaO <aoyewole2586@gmail.com>
2019-11-11 12:39:30 -0500

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function testFunc() {
 }
 
 function superbowlWin(record) {
+  // remember to save value to variable after the computer checks for element
+  // then add variable to if condition
+  // output of condition is stored in value and can return corresponding value
   let win = record.find(record => record.result === "W")
   if (win) {
     return win.year
