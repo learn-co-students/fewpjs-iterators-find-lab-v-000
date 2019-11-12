5960540cd7c5bd3d1b34fb18c64e0df5eac14a45
Jason Mei <jason.mei403@gmail.com>
2019-11-12 15:40:22 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = array => {
+  if (array.find(element => element.result === "W") !== undefined) {
+    return array.find(element => element.result === "W").year;
+  } else {
+    return undefined;
+  }
+}
