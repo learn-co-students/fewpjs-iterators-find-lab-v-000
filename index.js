2ecd91739107be96f7bd42ed9c9a488db7702857
Richard Cho <richardcho100@gmail.com>
2019-11-26 14:39:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+superbowlWin = (totalRecord) => {
+  let resultObj = totalRecord.find(record => record.result === 'W' || record.result === 'w')
+  return (!!resultObj ? resultObj.year : undefined)
 }
