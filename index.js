a2221fbf3039254e8c72e70dc47fd0229d2f00e6
Rebecca Tong <rtong@buffalo.edu>
2019-12-02 14:32:06 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(ob){
+  let football = ob.find(element => element.result === 'W')
+  if (football){
+  return football.year
+}
+else {
+  return undefined
+}
+
+}
