abac9b771e79889541086f8b0f62ce267834eaa7
Heather <heather.miller114@gmail.com>
2019-12-05 11:50:11 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let i = record.find( ({ result }) => result === 'W' );
+  if (i) {
+    return i.year
+  }
+}
