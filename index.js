632d15d3a28fddf79219f80ec35dc1c0a2f80b37
srijakumar <srijakumar7@gmail.com>
2019-12-12 16:07:00 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (record) => {
+  let test = record.find( record => record.result === "W" )
+  return !!test ? test.year : undefined
+}
