3320fa69a37f5e34bb8769e3b4b26396a8365ab9
Mohammed Noori <kmanoorib@gmail.com>
2020-01-02 04:02:00 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = function(wins) {
+  let found = wins.find( w => w.result === "W" )
+  if (!found) {
+    return undefined;
+  }
+  else {
+    return found.year;
+  }
+}
