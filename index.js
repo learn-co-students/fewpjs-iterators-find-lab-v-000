97b0880fd9e226662fb43d32e671ffdcec201072
Shaun McLoughlin <skmcloughlin@gmail.com>
2020-01-02 11:23:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const testVar = {}
+const testVar = {};
 
-function testFunc() {
-  return "hi"
+function isWon(obj) {
+  return obj.result === "W";
+}
+
+function superbowlWin(record) {
+  let selected = record.find(isWon);
+  if (!!selected) {
+    return selected.year;
+  } else {
+    return undefined;
+  }
 }
