4139d342e7d20f621172881d5cf53f96012952ac
MDAM182 <mdam2345@gmail.com>
2020-01-19 18:04:02 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(obj) {
+    const win = obj.find(e => e.result === 'W')//iterate over Object array/ pull the first key with a result value strongly equal to W
+  if (win){ // if the result is === to W proceed
+      return win.year // return the year key with the result value of W
+  };
+}
