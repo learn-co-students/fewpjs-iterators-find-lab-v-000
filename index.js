37c3f8c1b8fd14c7c7af90a36a33d89794665077
Sushi Chanrai <sushi@pathworks.co>
2020-01-24 13:45:20 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+    let found = record.find(item => {
+    return item.result === "W"
+    //if an es6 function goes over two lines you have to use return
+   })
+    return !!found ? found.year : undefined
+}
