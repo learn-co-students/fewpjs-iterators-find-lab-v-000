23c6770060799bc07a1b00881e84cf793f88611e
Meg Gutshall <meghan.gutshall@gmail.com>
2020-01-30 19:47:27 -0500

    Add alternate solution

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function superbowlWin(record) {
   function readStats(stat) { return stat.result === "W" ? stat : undefined };
 };
 
+// Flatiron solution
+
+// superbowlWin = (record) => {
+//   let result = record.find( record => record.result === "W" )
+//   return !!result ? result.year : undefined
+// }
+
+// Why couldn't I get this to work right for me?
