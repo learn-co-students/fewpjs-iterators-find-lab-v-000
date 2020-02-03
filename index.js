3ab93a6feda8683fe08f187c7a9dbea9c12cbb55
GiancarloZ <gzabaleta48@gmail.com>
2020-02-03 13:03:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
-}
-
 function superbowlWin(record){
   if (record.find(e => e.result === "W")){
     return record.find(e => e.result === "W").year
