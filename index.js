1e334174ae429f239ca52639ea21dfc572bd1f51
corytveten <corytveten@gmail.com>
2020-02-04 18:06:05 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objs) {
+  let answer = objs.find(x => x.result === "W");
+    return !!answer ? answer.year : undefined
+}
