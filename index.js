57230b72d9836d477a05f662d898bacc9987bdde
Bruce Hillsworth <brucehillsworth@Air.local>
2020-02-07 16:48:19 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  if (array.find(object => object.result === "W")) {
+    return array.find(object => object.result === "W").year;
+  } else {
+    return undefined;
+  };
+};
