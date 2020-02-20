f450712dc47c659b321d6c48812131b5ebeaf318
MiriamPeskowitz <miriam5000@gmail.com>
2020-02-20 11:29:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 
 
-// const record = [
-//   {year: "2018", result: "L"},
-//   {year: "2017", result: "W"},
-//   {year: "2016", result: "N/A"}
-// ]
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
 const superbowlWin = (record) => {
-	
 	const winYear = record.find(record => record.result === 'W' )
-	
-
-	// if (!!winYear) {
-	// 	return winYear.year
-	// } else {
-	// 	return undefined
-	// }
-	 return !!winYear ? winYear.year : undefined
+	return winYear ? winYear.year : undefined	
 }
+
+superbowlWin(record)
+
