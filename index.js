2c4e01692de4943e7019fe49411e78f1a0818933
William Barela <william.barela.dev@gmail.com>
2020-02-20 08:56:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
-}
-
 const superbowlWin = (records) => {
   const search = records.find(record => record.result === "W");
   return (search === undefined) ? undefined : search.year;
