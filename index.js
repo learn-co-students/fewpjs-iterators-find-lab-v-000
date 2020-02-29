12bbb4ca73bba3a0f097d73192e0f0f9f34af95f
Shanise Barona <shanisebarona@gmail.com>
2020-02-29 06:44:39 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arr) {
+  let found = arr.find(rec => rec.result === "W")
+  return found ? found.year : undefined
+}
