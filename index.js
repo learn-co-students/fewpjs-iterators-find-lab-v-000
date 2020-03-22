e1051918a6c8f6b6efe00d46cd69035821d69285
ElvinGarcia <dimix181@gmail.com>
2020-03-22 00:22:37 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(arg) {
+  if (arg.find(e => e.result === 'W'))
+   return arg.find(e => e.result === 'W').year
+}
+
+// function superbowlWin(arg) {
+//   const result = arg.find(e => e.result === 'W');
+//   result ? result.year : undefined
+// }
