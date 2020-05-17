58e33dbaf469003b49f4c83d385f6c23b671ac0d
sdcro <sdcrouse91@gmail.com>
2020-05-17 14:35:10 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(teamRecord) {
+  let gameWon = teamRecord.find( game => game.result === "W" );
+  
+  return gameWon ? gameWon.year : undefined;
+}
