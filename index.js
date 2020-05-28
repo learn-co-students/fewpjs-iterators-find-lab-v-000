b4c43eda1c5a272c119a84816d52e4e453ffc14c
sjwakeman <swakeman@ca.rr.com>
2020-05-27 19:31:52 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// Iterate over record array of Objects.
+// Find the result property that has a value of "W" in an Object
+// Get the year property in that Object.
+
+function superbowlWin(record) {
+  // console.log(record, "record") // Returns record ARRAYS of OBJECTS
+
+  //const result = inventory.find( ({ name }) => name === 'cherries' );
+  const win = record.find( ({ result }) => result === 'W')
+  //const win = record.find( ( info ) => info.result === 'W') // ANOTHER WAY TO SOLVE
+  //console.log(win, "win")   // { year: "1969", result: "W"},
+
+  return !!win ? win.year : undefined 
+}
