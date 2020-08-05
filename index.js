70a470ed658a7d1c2e9b0885e4086e3e1c0e9cdd
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-08-05 10:30:11 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function isWin(element, index, array) {
+  return (element.result === "W")
+}
+
+function superbowlWin(array) {
+  let result = array.find(isWin)
+  return !!result ? result.year : undefined
+}
