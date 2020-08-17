85e790c77f2a8901eda0cfe712ba66a5171d50bc
Liz P <parisielizabeth@gmail.com>
2020-08-17 03:28:32 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(records){
+  const win = records.find(record => record.result === "W");
+  return win ? win.year : undefined;
+}
