058c223c4d409ddb51583941950f4223bbf60bfe
ADogTheWarrior <coolfudge@gmail.com>
2020-08-31 12:20:23 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  if (array.find(s => s.result === "W") == undefined){
+    return undefined
+  }
+  else {
+    return array.find(s => s.result === "W").year
+  }
+}
