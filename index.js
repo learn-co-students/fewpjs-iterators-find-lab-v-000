abe7b6a35fbecdf2f5805f16d40dc85f3b6e5c1d
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-06 22:25:26 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = record => {
+  let result = record.find(element => element.result === "W")
+  return !!result ? result.year : result;
+}
