50c79f7b76b289d73469acc803db852d27869ee5
github username <d24dust@hotmail.com>
2021-03-11 13:20:00 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(winObjects) {
+  const result = winObjects.find(winObject=>winObject.result === "W")
+  return (result) ? result.year : result
+}
