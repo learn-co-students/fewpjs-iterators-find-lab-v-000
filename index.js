eb6b1b1f6ceb30f07bdd210c28313f748a0b91c6
github username <github email address>
2021-03-23 15:32:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const testVar = {}
 
 function testFunc() {
-  return "hi"
+    return "hi"
 }
 
-function superbowlWin(aryOfObjs){
-  let winner = aryOfObjs.find(obj=>obj.result==="W");
-  return (!!winner)? winner.year : winner;
+function superbowlWin(aryOfObjs) {
+    //   let winner = aryOfObjs.find(obj=>obj.result==="W");
+    //   return (!!winner)? winner.year : winner;
+
+    let win = aryOfObjs.find(win => win.result === "W")
+    return win && win.year
 }
