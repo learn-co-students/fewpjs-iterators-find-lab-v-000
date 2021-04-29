ad81632b4ab4a618f73853661eee1e90e245701a
Paul Fell <swellpf@gmail.com>
2021-04-29 10:46:47 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(data) {
+  let result = data.find(data => data.result === "W")
+  return !!result ? result.year : undefined
+}
