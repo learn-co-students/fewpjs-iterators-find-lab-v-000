dc1e243fa2a4ee75cc8913bba1d96ac266722fa1
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-16 15:25:54 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let game = record.find(game => game.result === "W");
+  // my solution
+  return game === undefined ? game : game.year;
+  // Flatiron solution - I keep forgetting about the !!
+  // return !!result ? result.year : undefined
+};
