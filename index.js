97dddb26503d3ed0fcc3a3d85ab47b8b503373b9
Thomas <Thomas.c.viles.jr@gmail.com>
2021-06-18 16:05:47 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let result = record.find(object => {
+    return object.result === "W"
+  })
+
+  return result ? result.year : undefined
+}
